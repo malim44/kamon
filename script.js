0c3dabd589f747b655328926e3e546c41fcb3ba5
malim44 <malim44@web.de>
2026-03-08 16:41:28 +0100

    add other areas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const add_todo = document.querySelector('.add-todo')
 const todo_text = document.querySelector('.todo-text')
-const todos_list = document.querySelector('.todos-list')
+const backlog = document.querySelector('.backlog')
 const shuffleBtn = document.querySelector('button')
 const todos = []
 
@@ -13,7 +13,7 @@ add_todo.addEventListener('click', (e) => {
     if(todo_text.value) {
         const todo_single = document.createElement('li')
         todo_single.innerHTML = todo_text.value
-        todos_list.appendChild(todo_single)
+        backlog.appendChild(todo_single)
         todo_text.value = ''
     }
 })
