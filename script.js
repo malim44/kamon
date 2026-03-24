a34a7c13447f2200c04ebc20c002de8fa5280507
malim44 <malim44@web.de>
2026-03-24 21:02:23 +0100

    tidy up

--- script.js
+++ script.js
@@ -7,11 +7,23 @@ createApp({
             active: [],
             backlog: []
             
+            title: '',
+            type: 'einmalig',
+            status: 'backlog',
+            timing: 'irgendwann',
+            date: this.getTomorrowDate(),
+
+            repeat: 'tage',
+            repeatValue: 1,
+            weekdays: [],
+
+            notes: '',
+            tomorrow: this.getTomorrowDate()    
         }
     },
     methods: {
         showTodoForm() {
-                    
+
         },
         addTodoToBacklog() {
             const todo = {
@@ -46,7 +58,71 @@ createApp({
             for(let i=this.active.length - 1; i >= 0; i--) {
                 this.active[i].coins -= 2
             }
+        },
+
+        getTomorrowDate() {
+          const today = new Date();
+          today.setDate(today.getDate() + 1);
+          return today.toISOString().split('T')[0];
+        },
+
+        toggleWeekday(day) {
+          if (this.weekdays.includes(day)) {
+            this.weekdays = this.weekdays.filter(d => d !== day);
+          } else {
+            this.weekdays.push(day);
+          }
+        },
+
+        cancel() {
+          this.title = '';
+          this.type = 'einmalig';
+          this.status = 'backlog';
+          this.timing = 'irgendwann';
+          this.date = this.getTomorrowDate();
+          this.repeat = 'tage';
+          this.repeatValue = 1;
+          this.weekdays = [];
+          this.notes = '';
+        },
+
+        createTodo() {
+          const todo = {
+            title: this.title,
+            type: this.type,
+            status: this.status,
+            timing: this.timing,
+            date: this.date,
+            repeat: this.repeat,
+            repeatValue: this.repeatValue,
+            weekdays: this.weekdays,
+            notes: this.notes
+          };
+
+          console.log("Todo erstellt:", todo);
+          alert("Todo erstellt! (siehe Konsole)");
+        },
+        watch: {
+        type(newVal) {
+          if (newVal !== 'einmalig') {
+            this.timing = 'irgendwann';
+            this.date = '';
+          } else {
+            this.date = this.getTomorrowDate();
+          }
+        },
+
+        timing(newVal) {
+          if (newVal === 'exakt' || newVal === 'spaetestens') {
+            if (!this.date) {
+              this.date = this.getTomorrowDate();
+            }
+          } else {
+            this.date = '';
+          }
         }
+      }
+
     },
     computed: {
         
